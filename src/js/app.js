3dab322460f8006797ae913b862cc9c00b71c3cd
Oleg Khvostenko <xboctehko@yandex.ru>
2023-08-14 15:54:34 +0200

    fixed webpack.common

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
+const counter = document.querySelector('.counter');
+const field = document.querySelector('.field');
 const cells = document.querySelectorAll('.cell');
 const goblin = document.createElement('img');
 goblin.src = '../static/img/goblin.png';
+goblin.classList = 'goblin';
 
 let currPos = 16;
 let newPos = 0;
+let count = 0;
 
 setInterval( () => {
   do {
@@ -12,3 +16,17 @@ setInterval( () => {
   cells[newPos].appendChild(goblin);
   currPos = newPos;
 }, 1000);
+
+field.addEventListener('click', (e) => {
+  if (e.target.classList[0] === 'goblin') {
+    counter.textContent = ++count;
+    parnt = e.target.parentElement;
+    parnt.style = 'background: green;';
+    setTimeout(() => {parnt.style = 'background: white;'}, 100);
+  } else {
+    count = 0;
+    counter.textContent = "0 - You're lose!!!";
+    e.target.style = 'background: red;';
+    setTimeout(() => {e.target.style = 'background: white;'}, 100);
+  }
+})
